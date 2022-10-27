6b19adc6a8824dc0468a299834c4e6e83a541ceb
brandon <bkappel16@gmail.com>
2022-10-27 08:32:40 -0500

    bcrypt

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { authUser, authRole } = require("../middleware/check-auth")
 const { nanoid } = require("nanoid");
