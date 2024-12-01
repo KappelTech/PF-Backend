7b6c88401ab9e2764c8a0218bcbbc3edea0a51d3
Brandon Kappel <bkappel16@gmail.com>
2024-11-30 20:53:54 -0600

    allow request from anywhere for now

--- src/app.ts
+++ src/app.ts
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 const corsOptions = {
-  origin: 'http://localhost:5173', // Allow requests from your frontend
+  origin: '*', // Allow requests from your frontend
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
   allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization, skipLoading',  // Allowed headers
   credentials: false, // Allow cookies if needed
