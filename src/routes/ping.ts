59ee88eb77dad7482b81c1637017a2b3261c09cf
Brandon Kappel <bkappel16@gmail.com>
2025-01-07 14:15:53 -0600

    bump

--- src/routes/ping.ts
+++ src/routes/ping.ts
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('',(req, res, next) => {
     res.status(201).json({
-        message: `Your're pinging, I'm here, i hope, yes!!!!`,
+        message: `Your're pinging, I'm here!!!!, hello?`,
     });
 })
 
