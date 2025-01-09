cd1bd859fe713801e2e7ef1531af3f76f8cd2e81
Brandon Kappel <bkappel16@gmail.com>
2025-01-08 20:39:55 -0600

    env's added

--- src/routes/ping.ts
+++ src/routes/ping.ts
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('',(req, res, next) => {
     res.status(201).json({
-        message: `Your're pinging, I'm here!!!!, helloo?`,
+        message: `Your're pinging, I'm here!!!!, helloo,? I'm here!`,
     });
 })
 
